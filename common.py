920ddc3b155e88cdcf87855190110c2147f563d0
Daniel Agar <daniel@agar.ca>
2013-02-21 23:30:00 -0500

    rename factor to factors

--- common.py
+++ common.py
@@ -24,5 +24,7 @@ def fib(n):
     return fn
 
 
-def factor(n):
-    return [i for i in xrange(1, n+1) if (n % i == 0)]
+def factors(n):
+    #return [i for i in xrange(1, int(n**0.5)+1) if (n % i == 0)]
+
+    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
