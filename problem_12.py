4a04b498c7f253535fb8b0e64d787c856128b816
Daniel Agar <daniel@agar.ca>
2013-02-21 23:34:48 -0500

    problem 12 finally correct

--- problem_12.py
+++ problem_12.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
-from common import factor
+from common import factors
 
 def triangle_number(n):
     return sum(xrange(n+1))
 
-x=1
-for n in xrange(x, x+10):
+for n in xrange(2, 100000):
     tn = triangle_number(n)
-    ln = len(factor(tn))
-    print n, tn, ln
+    ln = len(factors(tn))
+    #print n, tn, ln
     if ln > 100:
         print "Over 100 factors ", n, tn, ln
     if ln > 500:
-        print "Over 500 factors ", tn
+        print "Over 500 factors ", n, tn, ln
         break
