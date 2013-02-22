ea772f8ab87e9cac40ec17d00f37e52d98ea7c70
Daniel Agar <daniel@agar.ca>
2013-02-21 23:34:34 -0500

    much better factors function

--- common.py
+++ common.py
@@ -26,5 +26,4 @@ def fib(n):
 
 def factors(n):
     #return [i for i in xrange(1, int(n**0.5)+1) if (n % i == 0)]
-
     return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
