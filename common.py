709a979bd22bdf63f7ef4a3b0089cc0fb5d7b367
Daniel Agar <daniel@agar.ca>
2014-09-29 23:40:29 -0400

    update isprime

--- common.py
+++ common.py
@@ -1,9 +1,13 @@
 def isprime(n):
   '''check if integer n is a prime'''
   # range starts with 2 and only needs to go up the squareroot of n
+  if n == 1:
+      return False
+
   for x in xrange(2, int(n**0.5) + 1):
     if (n % x == 0):
       return False
+
   return True
 
 
